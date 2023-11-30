11f6292101a6a7d6cf8e2e4d5ca7fa990b571da7
Dan Sully <dan+github@sully.org>
2023-11-30 10:18:52 -0800

    Allow standard-ish environment variables for OpenAI.

--- src/application/cli.rs
+++ src/application/cli.rs
@@ -153,6 +153,7 @@ fn arg_model() -> Arg {
         .short('m')
         .long("model")
         .env("OATMEAL_MODEL")
+        .env("OPENAI_MODEL")
         .num_args(1)
         .help("The initial model on a backend to consume")
         .default_value("llama2:latest");
@@ -271,6 +272,8 @@ fn build() -> Command {
             Arg::new("openai-token")
                 .long("openai-token")
                 .env("OATMEAL_OPENAI_TOKEN")
+                .env("OPENAI_API_KEY")
+                .env("OPENAI_KEY")
                 .num_args(1)
                 .help("OpenAI API token when using the OpenAI backend")
                 .global(true),

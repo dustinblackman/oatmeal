dd2069074e0df12024ca4fce39b41a97fd5e40df
Dustin Blackman <dev@dustinblackman.com>
2023-12-20 21:30:27 -0500

    chore: OpenSSL to rustls

--- tools/thirdparty/run.sh
+++ tools/thirdparty/run.sh
@@ -4,12 +4,12 @@ set -e
 
 PROGDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 APPROVED_DEFAULT_LICENSES="$(cat "$PROGDIR"/approved_licenses.json | jq -rc 'to_entries[] | .value[]' | sd '\n' '|')ignoremeplz"
-LICENSES=$(cargo about generate -c "$PROGDIR/about.toml" "$PROGDIR/templates/json-nl.hbs" | grep -v -E "($APPROVED_DEFAULT_LICENSES)")
+LICENSES=$(cargo about generate -c "$PROGDIR/about.toml" "$PROGDIR/templates/json-nl.hbs")
 
 FAILED_LINT="false"
 
 function grepBadStrings() {
-	if (echo "$LICENSES" | grep -q "$1"); then
+	if (echo "$LICENSES" | grep -v -E "($APPROVED_DEFAULT_LICENSES)" | grep -q "$1"); then
 		pkgs=$(echo "$LICENSES" | grep "$1" | jq -rc '"\(.package_name_version) - \(.license) - \(.link)"')
 
 		echo "ERROR: Bad license found grepping for text: $1"
@@ -29,6 +29,18 @@ grepBadStrings "LICENSE-MIT or"
 grepBadStrings "apache"
 grepBadStrings "Apache"
 
+# Final sanity check that all cargo packages are in there.
+yq -p=toml -o=json "$PROGDIR/about.toml" | jq -rc '.targets[]' | while read target; do
+	cargo tree --target "$target" -e normal -f '{p}' | sd ' ' '\n' | grep '[a-zA-Z]' | grep -v '^v\d' | grep -v -E "(oatmeal|.git|(proc-macro))" | sort | uniq | while read f; do
+		echo "$LICENSES" | jq -rc '.package_name_version' | grep -q "^${f}--" || (echo "ERROR: Missing license $f for target $target" && FAILED_LINT="true")
+	done
+done
+
+# Verify there is no unused clarifications in about.toml
+cat "$PROGDIR/about.toml" | grep 'clarify' | sd '\[' '' | awk -F '.' '{print $1}' | sort | uniq | while read f; do
+	cat "$PROGDIR/../../Cargo.lock" | grep -q "$f" || (echo "ERROR: about.toml has unused clarification for $f" && FAILED_LINT="true")
+done
+
 if [[ "$FAILED_LINT" == "true" ]]; then
 	exit 1
 fi
@@ -36,19 +48,9 @@ fi
 # Cargo packages
 cargo about generate -c "$PROGDIR/about.toml" "$PROGDIR/templates/html.hbs" >"$PROGDIR/../../THIRDPARTY.html"
 
-# Final sanity check that all cargo packages are in there.
-yq -p=toml -o=json "$PROGDIR/about.toml" | jq -rc '.targets[]' | while read target; do
-	cargo tree --target "$target" -e normal -f '{p}' | sd ' ' '\n' | grep '[a-zA-Z]' | grep -v '^v\d' | grep -v -E "(oatmeal|.git|(proc-macro))" | sort | uniq | while read f; do
-		cat "$PROGDIR/../../THIRDPARTY.html" | grep -q "$f" || (echo "ERROR: Missing license $f for target $target" && FAILED_LINT="true")
-	done
-done
-
-if [[ "$FAILED_LINT" == "true" ]]; then
-	exit 1
-fi
-
 # Additional Third Party
-sd '__OPENSSL_LICENSE__' "$(curl -s -L https://raw.githubusercontent.com/sfackler/rust-openssl/48b8e81e807b6f589ae66fed9567084c3d833b83/THIRD_PARTY)" "$PROGDIR/../../THIRDPARTY.html"
+sd '__BETTERTLS_LICENSE__' "$(curl -s -L https://raw.githubusercontent.com/rustls/webpki/v/0.101.7/third-party/bettertls/LICENSE)" "$PROGDIR/../../THIRDPARTY.html"
+sd '__CHROMIUM_LICENSE__' "$(curl -s -L https://raw.githubusercontent.com/rustls/webpki/v/0.101.7/third-party/chromium/LICENSE)" "$PROGDIR/../../THIRDPARTY.html"
 
 # Themes
 sd '__BASE16_TEXTMATE__' "$(cat "$PROGDIR"/../../.cache/themes/LICENSE.md | awk 'NR > 1')" "$PROGDIR/../../THIRDPARTY.html"

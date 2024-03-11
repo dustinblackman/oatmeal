dcee6c5726281d3322969e9d82c163abe6ebb8f6
Dustin Blackman <github@dustinblackman.com>
2024-03-10 22:30:18 -0400

    chore: Shorten action run time for debugging

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -10,19 +10,21 @@ cd "$PROGDIR/.."
 	npm ci
 )
 
-echo "Building arm64"
+echo "[BUILD] Building arm64"
 tools/node/node_modules/.bin/devcontainer build --workspace-folder . --config ./.devcontainer/devcontainer-src.json --push --platform "linux/arm64" --image-name ghcr.io/dustinblackman/devcontainer-oatmeal:latest-arm64
-echo "Building amd64"
+echo "[BUILD] Building amd64"
 tools/node/node_modules/.bin/devcontainer build --workspace-folder . --config ./.devcontainer/devcontainer-src.json --push --platform "linux/amd64" --image-name ghcr.io/dustinblackman/devcontainer-oatmeal:latest-amd64
-echo "Creating manifest"
+echo "[BUILD] Creating manifest"
 docker manifest create ghcr.io/dustinblackman/devcontainer-oatmeal:latest ghcr.io/dustinblackman/devcontainer-oatmeal:latest-arm64 ghcr.io/dustinblackman/devcontainer-oatmeal:latest-amd64
-echo "Pushing manifest"
+echo "[BUILD] Pushing manifest"
 BUILD_SHA=$(docker manifest push ghcr.io/dustinblackman/devcontainer-oatmeal:latest)
 
+echo "[BUILD] Updating docker-compose.yml"
 DC_UPDATE=$(yq ".services.oatmeal.image = \"ghcr.io/dustinblackman/devcontainer-oatmeal@${BUILD_SHA}\"" ./.devcontainer/docker-compose.yml)
 rm ./.devcontainer/docker-compose.yml
 echo "$DC_UPDATE" >./.devcontainer/docker-compose.yml
 
+echo "[BUILD] Creating GitHub PR"
 git config --global user.email github-actions[bot]@users.noreply.github.com
 git config --global user.name github-actions[bot]
 git config pull.rebase false
@@ -34,3 +36,5 @@ git push origin "$BUILD_BRANCH"
 gh pr create \
 	--title "[CI] Update devcontainer to $BUILD_SHA" \
 	--body "Updates the dev container image based on the changes made in https://github.com/dustinblackman/oatmeal/commit/${GITHUB_SHA}"
+
+echo "[BUILD] Done"

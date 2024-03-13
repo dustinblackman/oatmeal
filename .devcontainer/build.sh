5c2ac60f7617e1e51caa82daa826679e0a372377
Dustin Blackman <github@dustinblackman.com>
2024-03-12 21:42:39 -0400

    chore: bring in zig

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -6,20 +6,19 @@ PROGDIR=$(dirname "$(readlink -f "$0")")
 cd "$PROGDIR/.."
 
 BUILD_ARCH="$1"
-export EMPTY_GITHUB_TOKEN="$2"
 
 (
 	cd tools/node
 	npm ci
 )
 
-echo "$EMPTY_GITHUB_TOKEN" | gh auth login --with-token
 sudo apt-get update
 sudo apt-get install gcc-aarch64-linux-gnu
 rustup target add aarch64-unknown-linux-gnu
 cargo install cargo-run-bin
 cargo binstall --help
 export CARGO_BUILD_TARGET=aarch64-unknown-linux-gnu
+export CC="$PROGDIR/zcc.sh"
 cargo bin committed --help || echo ""
 cargo bin mise --help || echo ""
 cargo cmd --help || echo ""
@@ -27,7 +26,8 @@ cargo nextest --help || echo ""
 cargo insta --help || echo ""
 cargo deny --helpA || echo ""
 cargo watch --help || echo ""
+rm -rf .bin/*/cargo-binstall
 find .bin
 
-# echo "[BUILD] Building $BUILD_ARCH"
-# tools/node/node_modules/.bin/devcontainer build --workspace-folder . --config ./.devcontainer/devcontainer-src.json --push --platform "linux/$BUILD_ARCH" --image-name ghcr.io/dustinblackman/devcontainer-oatmeal:latest-"$BUILD_ARCH"
+echo "[BUILD] Building $BUILD_ARCH"
+tools/node/node_modules/.bin/devcontainer build --workspace-folder . --config ./.devcontainer/devcontainer-src.json --push --platform "linux/$BUILD_ARCH" --image-name ghcr.io/dustinblackman/devcontainer-oatmeal:latest-"$BUILD_ARCH"
---
+++ .devcontainer/zcc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+zig cc -target aarch64-linux "$@"

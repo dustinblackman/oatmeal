e0b8f210b49b153f2e2a044da3f2c1b871e5792f
Dustin Blackman <github@dustinblackman.com>
2024-03-14 20:17:26 -0400

    chore: Remove bininstall

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -5,4 +5,3 @@ set -e
 rm -rf /workspace/.bin
 ln -s /tmp/build-cache/.bin /workspace/.bin
 ls /workspace/ | grep target | grep -q 'oatmeal' || sudo chown -R oatmeal:oatmeal /workspace/target
-cargo bin --install
